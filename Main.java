72831c1c3fb185d3a4f63705c30fd2563f07f4bd
ishnanurf <12450124280@students.uin-suska.ac.id>
2026-01-01 22:19:03 +0700

    Update songs in the playlist

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+    Playlist playlist = new Playlist();
+
+    playlist.addSong("imagination <shawn mendes>");
+    playlist.addSong("jangan cintai aku apa adanya <tulus>");
+    playlist.addSong("kusut <fourtwnty>");
+
+    playlist.displayPlaylist();
+  }
+}
